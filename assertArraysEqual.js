2ba0c46de14a5e180a0ff61d6287473dc2b7dff2
DanRoss88 <danielmartinross@gmail.com>
2023-04-04 11:10:24 -0700

    assert Array equal completed and touched up

---
+++ assertArraysEqual.js
@@ -0,0 +1,29 @@
+
+function eqArrays(a, b) {
+  if (a.length !== b.length) {
+    return false;
+  } for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i] || typeof a[i] !== typeof b[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));// => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should FAIL
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should FAIL
