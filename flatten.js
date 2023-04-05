db3beaf469597c7c873283dfe6170c39feaae0b0
DanRoss88 <danielmartinross@gmail.com>
2023-04-05 09:52:55 -0700

    flatten function

---
+++ flatten.js
@@ -0,0 +1,25 @@
+
+function eqArrays(a, b) {
+  if (a.length !== b.length) {
+    return false;
+  } for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i] || typeof a[i] !== typeof b[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const flatten = function(array) {
+ return array.flat(1);
+}
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]).join(','));
