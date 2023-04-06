c767995fd23c437e46313c5b763ba6a5af537ede
DanRoss88 <danielmartinross@gmail.com>
2023-04-05 17:59:10 -0700

    Letter positions done and found

---
+++ letterPositions.js
@@ -0,0 +1,37 @@
+function eqArrays(a, b) {
+  if (a.length !== b.length) {
+    return false;
+  } for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i] || typeof a[i] !== typeof b[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArrayEquals = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+// consider countOnly function, and add in another section to the array for index
+const letterPositions = function (sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    let letter = sentence[i];
+    if (letter !== " ") {
+      if (results[letter]) {
+        results[letter].push(i);
+      } else {
+        results[letter] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+
+console.log(letterPositions("lighthouse in the house"));
+console.log(assertArrayEquals(letterPositions("hello").e, [1]));
