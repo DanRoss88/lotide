b8872e27c23b814a4c96e83ff467ea5cdea89cfa
DanRoss88 <danielmartinross@gmail.com>
2023-04-05 20:20:08 -0700

    omg im so tired, objects are equal or not assertively

---
+++ assertObjectsEqual.js
@@ -0,0 +1,36 @@
+const eqObjects = function (object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (const key of keys1) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+/*
+Implement assertObjectsEqual which will take in two objects and console.log an appropriate message to the console.
+*/
+
+const assertObjectsEqual = function (actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🚫 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+console.log()
