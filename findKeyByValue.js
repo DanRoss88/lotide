21e395dfbe07234bca1e608fe2b3b9510df99890
DanRoss88 <danielmartinross@gmail.com>
2023-04-05 18:22:44 -0700

    object keys finding value

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function (actual, expected) {
+  return actual === expected
+    ? `✅✅✅✅ Assertion Passed: ${actual} === ${expected}`
+    : `🚫🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`;
+};
+console.log(assertEqual("You did ", "You did it!"));
+console.log(assertEqual(16, 16));
+
+/*
+Implement the function findKeyByValue which takes in an object and a value. It should scan the object and return the first key which contains the given value. If no key with that given value is found, then it should return undefined.
+*/
+const findKeyByValue = function (obj, val) {
+  for (const key of Object.keys(obj)) {
+    if (obj[key] === val) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"));
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined));
